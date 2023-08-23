f668693ca1172e3b2a8c95b3ca54862b2340c0ae
Tajamul <tajamulali86@gmail.com>
2023-08-23 14:01:36 +0530

    added register/login to nav bar, separated home-partials added coma

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.main')
 @section('content')
-@include('home-partial.hero')
-@include('home-partial.step')
+@include('home-partials.hero')
+@include('home-partials.step')
 @endsection
